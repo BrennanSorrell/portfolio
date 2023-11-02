a8a93aaafe61a9dd022925406f003022e4dd4d42
BrennanSorrell <149116464+BrennanSorrell@users.noreply.github.com>
2023-11-02 17:02:10 -0400

    Add files via upload

---
+++ Program3/Program3/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace Program3
+{
+    partial class Program3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CityLbl = new System.Windows.Forms.Label();
+            this.EntreeItemNumberLbl = new System.Windows.Forms.Label();
+            this.QuantityLbl = new System.Windows.Forms.Label();
+            this.CalcButton = new System.Windows.Forms.Button();
+            this.InititalCostLbl = new System.Windows.Forms.Label();
+            this.AdjustedCostLbl = new System.Windows.Forms.Label();
+            this.ShipmentCostLbl = new System.Windows.Forms.Label();
+            this.TotalPriceLbl = new System.Windows.Forms.Label();
+            this.CityComboBox = new System.Windows.Forms.ComboBox();
+            this.EntreeItemNumberInput = new System.Windows.Forms.TextBox();
+            this.QuantityInput = new System.Windows.Forms.TextBox();
+            this.InititalCostOutput = new System.Windows.Forms.Label();
+            this.AdjustedCostOutput = new System.Windows.Forms.Label();
+            this.ShipmentCostOutput = new System.Windows.Forms.Label();
+            this.TotalPriceOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CityLbl
+            // 
+            this.CityLbl.AutoSize = true;
+            this.CityLbl.Location = new System.Drawing.Point(86, 15);
+            this.CityLbl.Name = "CityLbl";
+            this.CityLbl.Size = new System.Drawing.Size(27, 13);
+            this.CityLbl.TabIndex = 0;
+            this.CityLbl.Text = "City:";
+            // 
+            // EntreeItemNumberLbl
+            // 
+            this.EntreeItemNumberLbl.AutoSize = true;
+            this.EntreeItemNumberLbl.Location = new System.Drawing.Point(7, 45);
+            this.EntreeItemNumberLbl.Name = "EntreeItemNumberLbl";
+            this.EntreeItemNumberLbl.Size = new System.Drawing.Size(106, 13);
+            this.EntreeItemNumberLbl.TabIndex = 1;
+            this.EntreeItemNumberLbl.Text = "Entree/Item Number:";
+            // 
+            // QuantityLbl
+            // 
+            this.QuantityLbl.AutoSize = true;
+            this.QuantityLbl.Location = new System.Drawing.Point(14, 77);
+            this.QuantityLbl.Name = "QuantityLbl";
+            this.QuantityLbl.Size = new System.Drawing.Size(99, 13);
+            this.QuantityLbl.TabIndex = 2;
+            this.QuantityLbl.Text = "Quantity (Servings):";
+            // 
+            // CalcButton
+            // 
+            this.CalcButton.Location = new System.Drawing.Point(139, 100);
+            this.CalcButton.Name = "CalcButton";
+            this.CalcButton.Size = new System.Drawing.Size(75, 23);
+            this.CalcButton.TabIndex = 3;
+            this.CalcButton.Text = "Calculate";
+            this.CalcButton.UseVisualStyleBackColor = true;
+            this.CalcButton.Click += new System.EventHandler(this.CalcButton_Click);
+            // 
+            // InititalCostLbl
+            // 
+            this.InititalCostLbl.AutoSize = true;
+            this.InititalCostLbl.Location = new System.Drawing.Point(52, 171);
+            this.InititalCostLbl.Name = "InititalCostLbl";
+            this.InititalCostLbl.Size = new System.Drawing.Size(61, 13);
+            this.InititalCostLbl.TabIndex = 4;
+            this.InititalCostLbl.Text = "Initital Cost:";
+            // 
+            // AdjustedCostLbl
+            // 
+            this.AdjustedCostLbl.AutoSize = true;
+            this.AdjustedCostLbl.Location = new System.Drawing.Point(38, 208);
+            this.AdjustedCostLbl.Name = "AdjustedCostLbl";
+            this.AdjustedCostLbl.Size = new System.Drawing.Size(75, 13);
+            this.AdjustedCostLbl.TabIndex = 5;
+            this.AdjustedCostLbl.Text = "Adjusted Cost:";
+            // 
+            // ShipmentCostLbl
+            // 
+            this.ShipmentCostLbl.AutoSize = true;
+            this.ShipmentCostLbl.Location = new System.Drawing.Point(35, 245);
+            this.ShipmentCostLbl.Name = "ShipmentCostLbl";
+            this.ShipmentCostLbl.Size = new System.Drawing.Size(78, 13);
+            this.ShipmentCostLbl.TabIndex = 6;
+            this.ShipmentCostLbl.Text = "Shipment Cost:";
+            // 
+            // TotalPriceLbl
+            // 
+            this.TotalPriceLbl.AutoSize = true;
+            this.TotalPriceLbl.Location = new System.Drawing.Point(52, 282);
+            this.TotalPriceLbl.Name = "TotalPriceLbl";
+            this.TotalPriceLbl.Size = new System.Drawing.Size(61, 13);
+            this.TotalPriceLbl.TabIndex = 7;
+            this.TotalPriceLbl.Text = "Total Price:";
+            // 
+            // CityComboBox
+            // 
+            this.CityComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CityComboBox.FormattingEnabled = true;
+            this.CityComboBox.Items.AddRange(new object[] {
+            "Louisville",
+            "Lexington",
+            "Indianapolis",
+            "Nashville"});
+            this.CityComboBox.Location = new System.Drawing.Point(119, 12);
+            this.CityComboBox.Name = "CityComboBox";
+            this.CityComboBox.Size = new System.Drawing.Size(121, 21);
+            this.CityComboBox.TabIndex = 8;
+            // 
+            // EntreeItemNumberInput
+            // 
+            this.EntreeItemNumberInput.Location = new System.Drawing.Point(119, 42);
+            this.EntreeItemNumberInput.Name = "EntreeItemNumberInput";
+            this.EntreeItemNumberInput.Size = new System.Drawing.Size(121, 20);
+            this.EntreeItemNumberInput.TabIndex = 9;
+            // 
+            // QuantityInput
+            // 
+            this.QuantityInput.Location = new System.Drawing.Point(119, 74);
+            this.QuantityInput.Name = "QuantityInput";
+            this.QuantityInput.Size = new System.Drawing.Size(121, 20);
+            this.QuantityInput.TabIndex = 10;
+            // 
+            // InititalCostOutput
+            // 
+            this.InititalCostOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InititalCostOutput.Location = new System.Drawing.Point(119, 170);
+            this.InititalCostOutput.Name = "InititalCostOutput";
+            this.InititalCostOutput.Size = new System.Drawing.Size(121, 20);
+            this.InititalCostOutput.TabIndex = 11;
+            // 
+            // AdjustedCostOutput
+            // 
+            this.AdjustedCostOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AdjustedCostOutput.Location = new System.Drawing.Point(119, 207);
+            this.AdjustedCostOutput.Name = "AdjustedCostOutput";
+            this.AdjustedCostOutput.Size = new System.Drawing.Size(121, 20);
+            this.AdjustedCostOutput.TabIndex = 12;
+            // 
+            // ShipmentCostOutput
+            // 
+            this.ShipmentCostOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ShipmentCostOutput.Location = new System.Drawing.Point(119, 244);
+            this.ShipmentCostOutput.Name = "ShipmentCostOutput";
+            this.ShipmentCostOutput.Size = new System.Drawing.Size(121, 20);
+            this.ShipmentCostOutput.TabIndex = 13;
+            // 
+            // TotalPriceOutput
+            // 
+            this.TotalPriceOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TotalPriceOutput.Location = new System.Drawing.Point(119, 281);
+            this.TotalPriceOutput.Name = "TotalPriceOutput";
+            this.TotalPriceOutput.Size = new System.Drawing.Size(121, 20);
+            this.TotalPriceOutput.TabIndex = 14;
+            // 
+            // Program3
+            // 
+            this.AcceptButton = this.CalcButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 320);
+            this.Controls.Add(this.TotalPriceOutput);
+            this.Controls.Add(this.ShipmentCostOutput);
+            this.Controls.Add(this.AdjustedCostOutput);
+            this.Controls.Add(this.InititalCostOutput);
+            this.Controls.Add(this.QuantityInput);
+            this.Controls.Add(this.EntreeItemNumberInput);
+            this.Controls.Add(this.CityComboBox);
+            this.Controls.Add(this.TotalPriceLbl);
+            this.Controls.Add(this.ShipmentCostLbl);
+            this.Controls.Add(this.AdjustedCostLbl);
+            this.Controls.Add(this.InititalCostLbl);
+            this.Controls.Add(this.CalcButton);
+            this.Controls.Add(this.QuantityLbl);
+            this.Controls.Add(this.EntreeItemNumberLbl);
+            this.Controls.Add(this.CityLbl);
+            this.Name = "Program3";
+            this.Text = "uThankItWeMakeIt Cost Calculator";
+            this.Load += new System.EventHandler(this.Program3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label CityLbl;
+        private System.Windows.Forms.Label EntreeItemNumberLbl;
+        private System.Windows.Forms.Label QuantityLbl;
+        private System.Windows.Forms.Button CalcButton;
+        private System.Windows.Forms.Label InititalCostLbl;
+        private System.Windows.Forms.Label AdjustedCostLbl;
+        private System.Windows.Forms.Label ShipmentCostLbl;
+        private System.Windows.Forms.Label TotalPriceLbl;
+        private System.Windows.Forms.ComboBox CityComboBox;
+        private System.Windows.Forms.TextBox EntreeItemNumberInput;
+        private System.Windows.Forms.TextBox QuantityInput;
+        private System.Windows.Forms.Label InititalCostOutput;
+        private System.Windows.Forms.Label AdjustedCostOutput;
+        private System.Windows.Forms.Label ShipmentCostOutput;
+        private System.Windows.Forms.Label TotalPriceOutput;
+    }
+}
+
---
+++ Program3/Program3/Form1.cs
@@ -0,0 +1,181 @@
+﻿// Grading ID: S5040
+// Program 3
+// Due Nov 11th
+// CIS 199-50-4218
+// This program computes the prices for a company who sells food.
+using System;
+using System.Windows.Forms;
+
+namespace Program3
+{
+    public partial class Program3 : Form
+    {
+        public Program3()
+        {
+            InitializeComponent();
+        }
+
+        private void Program3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalcButton_Click(object sender, EventArgs e)
+        {
+            if (CityComboBox.SelectedIndex < 0) // checking to see if city was picked in the combo box
+            {
+                MessageBox.Show("Please Select A City!"); // message to tell them to pick a city
+            }
+            int itemNum, servings = 0; // declaring int to = 0, to use in statements if the wrong/no number is entered
+            string item = EntreeItemNumberInput.Text; // setting item to a string and getting the item number from what the user entered
+            bool successItem = Int32.TryParse(item, out itemNum); // setting successItem to a bool and parseing item to get the int itemNum
+            string quantity = QuantityInput.Text; // setting quantity to a string and getting the input from the user
+            bool successQuantity = Int32.TryParse(quantity, out servings); // setting successQuantity to a bool and parseing quantity to get the int servings
+            double inititalCost, servingNum; // declaring doubles
+            if (successItem) // seeing if the successItem variable to know if they entered a number
+            {
+            }
+            else
+            {
+                MessageBox.Show("Please Enter A Valid Entree/Item Number! (Numbers Only)"); // shows if there is nothing entered / letters entered
+            }
+            if (successQuantity) // seeing if the successQuantity variable to know if they entered a number
+            {
+            }
+            else
+            {
+                MessageBox.Show("Please Enter A Valid Quantity! (Mumbers Only)"); // shows if there is nothing entered / lettered entered
+            }
+            if (itemNum < 10001 || itemNum > 10007) // ensures the number is acceptable on the menu
+            {
+                MessageBox.Show("Please Enter A Number 10001-10007!"); // shows if number is not accepetable
+            }
+            if (servings <= 0) // ensures the number of servings is acceptable
+            {
+                MessageBox.Show("Please Enter A Number Above 0!"); // shows if number is not acceptable
+            }
+            servingNum = Convert.ToDouble(QuantityInput.Text); // gathering the servings from input
+            int[] entreeNum = new int[7] { 10001, 10002, 10003, 10004, 10005, 10006, 10007 }; // parralel array for the entree num
+            double[] entreePrice = new double[7] { 7.87, 9.51, 10.73, 9.99, 11.99, 5, 4.58 }; // parralel array for the entree price
+            int itemOrdered; // delcaring int
+            double itemPrice = 0; // declaring itemPrice = 0
+            bool isValidItem = false; // setting a bool up to make sure item is valid
+            double adjustedCost; // declaring double
+            const double serviceFee5 = 0.15; // setting const for the service fees
+            const double serviceFee10 = 0.10; // setting const for the service fees
+            const double serviceFee20 = 0.05; // setting const for the service fees
+            itemOrdered = Convert.ToInt32(EntreeItemNumberInput.Text); // getting entree num from input from user
+            for (int x = 0; x < entreeNum.Length; x++) // matching what was entered in the array
+            {
+                if (itemOrdered == entreeNum[x]) // matching what was entered in the array
+                {
+                    isValidItem = true; // setting the bool to true
+                    itemPrice = entreePrice[x]; // turning the matched quantity into a variable i can use
+                }
+            }
+            if (isValidItem) // since they valid item is true
+            {
+                inititalCost = itemPrice * servingNum; // we get the initialCost from multiplying item price by the serving number
+                InititalCostOutput.Text = $"{inititalCost:C}"; // we display the initial cost as currency in the output label
+                if (servingNum >= 0 && servingNum <= 5) // determing the serving size for the fees
+                {
+                    adjustedCost = inititalCost * serviceFee5; // finding the adjusted cost for 0-5 servings
+                    double a = adjustedCost + inititalCost; // doubling a to display the adjusted cost
+                    AdjustedCostOutput.Text = $"{a:C}"; // diplaying the adjustedcost into the output label
+                    string[] city = new string[4] { "Louisville", "Lexington", "Indianapolis", "Nashville" }; // making arrays for the combo box input city
+                    double[] shipmentFee = new double[4] { .06, 0.0717, 0.07, 0.0874 }; // making the array for the city shipment fees
+                    double shipmentFeeTotal; // declaring double
+                    string statePicked; // declaring string
+                    double shipmentFees, totalCost; // declaring double
+                    bool isValidState = false; // setting the bool isValidState to false
+                    statePicked = CityComboBox.Text; // getting the string statePicked from the combo box
+                    for (int i = 0; i < city.Length; i++) // finding the inputed city to match the array and get the shipment fees
+                    {
+                        if (statePicked == city[i]) // matching the city picked
+                        {
+                            isValidState = true; // setting valid state to true
+                            shipmentFees = shipmentFee[i]; // setting shipmentfee to the double shipment fees which i can use
+                            shipmentFeeTotal = a * shipmentFees; // getting the shipment fee total from adjusted cost * the shipment fees
+                            ShipmentCostOutput.Text = $"{shipmentFeeTotal:C}"; // displaying the shipment fee total into the label output box
+                            totalCost = a + shipmentFeeTotal; // calculating total cost
+                            TotalPriceOutput.Text = $"{totalCost:C}"; // displaying the total cost into the output label box
+                        }
+                    }
+                }
+                else if (servingNum >= 6 && servingNum <= 10) // determing the serving size for the fees
+                {
+                    adjustedCost = inititalCost * serviceFee10; // finding the adjusted cost for 6-10 servings
+                    double a = adjustedCost + inititalCost; // doubling a to display the adjusted cost
+                    AdjustedCostOutput.Text = $"{a:C}"; // diplaying the adjustedcost into the output label
+                    string[] city = new string[4] { "Louisville", "Lexington", "Indianapolis", "Nashville" }; // making arrays for the combo box input city
+                    double[] shipmentFee = new double[4] { .06, 0.0717, 0.07, 0.0874 }; // making the array for the city shipment fees
+                    double shipmentFeeTotal; // declaring double
+                    string statePicked; // declaring string
+                    double shipmentFees, totalCost; // declaring double
+                    bool isValidState = false; // setting the bool isValidState to false
+                    statePicked = CityComboBox.Text; // getting the string statePicked from the combo box
+                    for (int i = 0; i < city.Length; i++) // finding the inputed city to match the array and get the shipment fees
+                    {
+                        if (statePicked == city[i]) // matching the city picked
+                        {
+                            isValidState = true; // setting valid state to true
+                            shipmentFees = shipmentFee[i]; // setting shipmentfee to the double shipment fees which i can use
+                            shipmentFeeTotal = a * shipmentFees; // getting the shipment fee total from adjusted cost * the shipment fees
+                            ShipmentCostOutput.Text = $"{shipmentFeeTotal:C}"; // displaying the shipment fee total into the label output box
+                            totalCost = a + shipmentFeeTotal; // calculating total cost
+                            TotalPriceOutput.Text = $"{totalCost:C}"; // displaying the total cost into the output label box
+                        }
+                    }
+                }
+                else if (servingNum >= 11 && servingNum <= 20) // determing the serving size for the fees
+                {
+                    adjustedCost = inititalCost * serviceFee20; // finding the adjusted cost for 11-20 servings
+                    double a = adjustedCost + inititalCost; // doubling a to display the adjusted cost
+                    AdjustedCostOutput.Text = $"{a:C}"; // diplaying the adjustedcost into the output label
+                    string[] city = new string[4] { "Louisville", "Lexington", "Indianapolis", "Nashville" }; // making arrays for the combo box input city
+                    double[] shipmentFee = new double[4] { .06, 0.0717, 0.07, 0.0874 }; // making the array for the city shipment fees
+                    double shipmentFeeTotal; // declaring double
+                    string statePicked; // declaring string
+                    double shipmentFees, totalCost; // declaring double
+                    bool isValidState = false; // setting the bool isValidState to false
+                    statePicked = CityComboBox.Text; // getting the string statePicked from the combo box
+                    for (int i = 0; i < city.Length; i++) // finding the inputed city to match the array and get the shipment fees
+                    {
+                        if (statePicked == city[i]) // matching the city picked
+                        {
+                            isValidState = true; // setting valid state to true
+                            shipmentFees = shipmentFee[i]; // setting shipmentfee to the double shipment fees which i can use
+                            shipmentFeeTotal = a * shipmentFees; // getting the shipment fee total from adjusted cost * the shipment fees
+                            ShipmentCostOutput.Text = $"{shipmentFeeTotal:C}"; // displaying the shipment fee total into the label output box
+                            totalCost = a + shipmentFeeTotal; // calculating total cost
+                            TotalPriceOutput.Text = $"{totalCost:C}"; // displaying the total cost into the output label box
+                        }
+                    }
+                }
+                else if (servingNum >= 21) // determing the serving size for the fees
+                {
+                    AdjustedCostOutput.Text = $"{inititalCost:C}"; // diplaying the adjustedcost into the output label
+                    string[] city = new string[4] { "Louisville", "Lexington", "Indianapolis", "Nashville" }; // making arrays for the combo box input city
+                    double[] shipmentFee = new double[4] { .06, 0.0717, 0.07, 0.0874 }; // making the array for the city shipment fees
+                    double shipmentFeeTotal; // declaring double
+                    string statePicked; // declaring string
+                    double shipmentFees, totalCost; // declaring double
+                    bool isValidState = false; // setting the bool isValidState to false
+                    statePicked = CityComboBox.Text; // getting the string statePicked from the combo box
+                    for (int i = 0; i < city.Length; i++) // finding the inputed city to match the array and get the shipment fees
+                    {
+                        if (statePicked == city[i]) // matching the city picked
+                        {
+                            isValidState = true; // setting valid state to true
+                            shipmentFees = shipmentFee[i]; // setting shipmentfee to the double shipment fees which i can use
+                            shipmentFeeTotal = inititalCost * shipmentFees; // getting the shipment fee total from adjusted cost * the shipment fees
+                            ShipmentCostOutput.Text = $"{shipmentFeeTotal:C}"; // displaying the shipment fee total into the label output box
+                            totalCost = inititalCost + shipmentFeeTotal; // calculating total cost
+                            TotalPriceOutput.Text = $"{totalCost:C}"; // displaying the total cost into the output label box
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
